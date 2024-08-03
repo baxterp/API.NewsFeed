e3ff562bbc3f3e309db7fb55092fe9d25af9675d
Baxter Pearson <baxterrpearson@gmail.com>
2024-08-03 10:27:03 +0100

    Update NewsController routes and caching mechanisms
    - Updated `GetF1News`, `GetWECNews`, and `GetMotoGPNews` methods to support optional `numberOfRecords` and `numberOfDays` parameters.
    - Renamed `GetCryptoNews` to `GetF1News` and updated its route.
    - Removed `GetFashionNews` method.
    - Implemented caching with keys that include `numberOfRecords` and `numberOfDays`.
    - Added logic to filter and limit results based on `numberOfRecords` and `numberOfDays`.

--- API.NewsFeed/Controllers/NewsController.cs
+++ API.NewsFeed/Controllers/NewsController.cs
@@ -19,102 +19,11 @@ namespace API.NewsFeed.Controllers
         }
 
         [HttpGet]
-        [Route("f1news")]
-        public async Task<IActionResult> GetF1News()
-        {
-            string cacheKey = "F1News";
-            if (!_cache.TryGetValue(cacheKey, out List<Item> result))
-            {
-                string currentDirectory = Directory.GetCurrentDirectory();
-                IEnumerable<string> feeds = System.IO.File.ReadAllLines(currentDirectory + @"\Feeds\F1News.txt");
-
-                result = await RSSReader.ReadRSSFeeds(currentDirectory, "Formula 1", feeds);
-
-                var cacheEntryOptions = new MemoryCacheEntryOptions
-                {
-                    AbsoluteExpirationRelativeToNow = CacheDuration
-                };
-
-                _cache.Set(cacheKey, result, cacheEntryOptions);
-            }
-            var orderedResult = result?.OrderByDescending(o => o.PubDate).AsEnumerable();
-            return Ok(new Dictionary<string, IEnumerable<Item>> { { "F1News", orderedResult ?? new List<Item>() } });
-        }
-
-        [HttpGet]
-        [Route("wecnews")]
-        public async Task<IActionResult> GetWECNews()
-        {
-            string cacheKey = "WECNews";
-            if (!_cache.TryGetValue(cacheKey, out List<Item> result))
-            {
-                string currentDirectory = Directory.GetCurrentDirectory();
-                IEnumerable<string> feeds = System.IO.File.ReadAllLines(currentDirectory + @"\Feeds\WecNews.txt");
-
-                result = await RSSReader.ReadRSSFeeds(currentDirectory, "WEC", feeds) ?? new();
-
-                var cacheEntryOptions = new MemoryCacheEntryOptions
-                {
-                    AbsoluteExpirationRelativeToNow = CacheDuration
-                };
-
-                _cache.Set(cacheKey, result, cacheEntryOptions);
-            }
-            var orderedResult = result?.OrderByDescending(o => o.PubDate).AsEnumerable();
-            return Ok(new Dictionary<string, IEnumerable<Item>> { { "WECNews", orderedResult ?? new List<Item>() } });
-        }
-
-        [HttpGet]
-        [Route("motogpnews")]
-        public async Task<IActionResult> GetMotoGPNews()
-        {
-            string cacheKey = "MotoGPNews";
-            if (!_cache.TryGetValue(cacheKey, out List<Item> result))
-            {
-                string currentDirectory = Directory.GetCurrentDirectory();
-                IEnumerable<string> feeds = System.IO.File.ReadAllLines(currentDirectory + @"\Feeds\MotoGPNews.txt");
-
-                result = await RSSReader.ReadRSSFeeds(currentDirectory, "MotoGP", feeds) ?? new();
-
-                var cacheEntryOptions = new MemoryCacheEntryOptions
-                {
-                    AbsoluteExpirationRelativeToNow = CacheDuration
-                };
-
-                _cache.Set(cacheKey, result, cacheEntryOptions);
-            }
-            var orderedResult = result?.OrderByDescending(o => o.PubDate).AsEnumerable();
-            return Ok(new Dictionary<string, IEnumerable<Item>> { { "MotoGPNews", orderedResult ?? new List<Item>() } });
-        }
-
-        [HttpGet]
-        [Route("fashionnews")]
-        public async Task<IActionResult> GetFashionNews()
-        {
-            string cacheKey = "FashionNews";
-            if (!_cache.TryGetValue(cacheKey, out List<Item> result))
-            {
-                string currentDirectory = Directory.GetCurrentDirectory();
-                IEnumerable<string> feeds = System.IO.File.ReadAllLines(currentDirectory + @"\Feeds\FashionNews.txt");
-
-                result = await RSSReader.ReadRSSFeeds(currentDirectory, "FashionNews", feeds) ?? new();
-
-                var cacheEntryOptions = new MemoryCacheEntryOptions
-                {
-                    AbsoluteExpirationRelativeToNow = CacheDuration
-                };
-
-                _cache.Set(cacheKey, result, cacheEntryOptions);
-            }
-            var orderedResult = result?.OrderByDescending(o => o.PubDate).AsEnumerable();
-            return Ok(new Dictionary<string, IEnumerable<Item>> { { "FashionNews", orderedResult ?? new List<Item>() } });
-        }
-
-        [HttpGet]
         [Route("cryptonews")]
-        public async Task<IActionResult> GetCryptoNews()
+        [Route("cryptonews/{numberOfRecords}/{numberOfDays}")]
+        public async Task<IActionResult> GetCryptoNews(int numberOfRecords = 0, int numberOfDays = 0)
         {
-            string cacheKey = "CryptoNews";
+            string cacheKey = "CryptoNews" + numberOfRecords + numberOfDays;
             if (!_cache.TryGetValue(cacheKey, out List<Item> result))
             {
                 string currentDirectory = Directory.GetCurrentDirectory();
@@ -130,7 +39,115 @@ namespace API.NewsFeed.Controllers
                 _cache.Set(cacheKey, result, cacheEntryOptions);
             }
             var orderedResult = result?.OrderByDescending(o => o.PubDate).AsEnumerable();
+
+            if (numberOfRecords > 0)
+                orderedResult = orderedResult?.Take(numberOfRecords);
+            else if (numberOfDays > 0)
+                orderedResult = orderedResult?
+                    .Where(o => o.PubDate > DateTime.Now - new TimeSpan(numberOfDays, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second));
+            else
+                orderedResult = orderedResult?.Take(10);
+
             return Ok(new Dictionary<string, IEnumerable<Item>> { { "CryptoNews", orderedResult ?? new List<Item>() } });
+        }
+
+        [HttpGet]
+        [Route("f1news")]
+        [Route("f1news/{numberOfRecords}/{numberOfDays}")]
+        public async Task<IActionResult> GetF1News(int numberOfRecords = 0, int numberOfDays = 0)
+        {
+            string cacheKey = "F1News" + numberOfRecords + numberOfDays;
+            if (!_cache.TryGetValue(cacheKey, out List<Item> result))
+            {
+                string currentDirectory = Directory.GetCurrentDirectory();
+                IEnumerable<string> feeds = System.IO.File.ReadAllLines(currentDirectory + @"\Feeds\F1News.txt");
+
+                result = await RSSReader.ReadRSSFeeds(currentDirectory, "Formula 1", feeds);
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = CacheDuration
+                };
+
+                _cache.Set(cacheKey, result, cacheEntryOptions);
+            }
+            var orderedResult = result?.OrderByDescending(o => o.PubDate).AsEnumerable();
+
+            if (numberOfRecords > 0)
+                orderedResult = orderedResult?.Take(numberOfRecords);
+            else if (numberOfDays > 0)
+                orderedResult = orderedResult?
+                    .Where(o => o.PubDate > DateTime.Now - new TimeSpan(numberOfDays, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second));
+            else
+                orderedResult = orderedResult?.Take(10);
+
+            return Ok(new Dictionary<string, IEnumerable<Item>> { { "F1News", orderedResult ?? new List<Item>() } });
+        }
+
+        [HttpGet]
+        [Route("wecnews")]
+        [Route("wecnews/{numberOfRecords}/{numberOfDays}")]
+        public async Task<IActionResult> GetWECNews(int numberOfRecords = 0, int numberOfDays = 0)
+        {
+            string cacheKey = "WECNews" + numberOfRecords + numberOfDays;
+            if (!_cache.TryGetValue(cacheKey, out List<Item> result))
+            {
+                string currentDirectory = Directory.GetCurrentDirectory();
+                IEnumerable<string> feeds = System.IO.File.ReadAllLines(currentDirectory + @"\Feeds\WecNews.txt");
+
+                result = await RSSReader.ReadRSSFeeds(currentDirectory, "WEC", feeds) ?? new();
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = CacheDuration
+                };
+
+                _cache.Set(cacheKey, result, cacheEntryOptions);
+            }
+            var orderedResult = result?.OrderByDescending(o => o.PubDate).AsEnumerable();
+
+            if (numberOfRecords > 0)
+                orderedResult = orderedResult?.Take(numberOfRecords);
+            else if (numberOfDays > 0)
+                orderedResult = orderedResult?
+                    .Where(o => o.PubDate > DateTime.Now - new TimeSpan(numberOfDays, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second));
+            else
+                orderedResult = orderedResult?.Take(10);
+
+            return Ok(new Dictionary<string, IEnumerable<Item>> { { "WECNews", orderedResult ?? new List<Item>() } });
+        }
+
+        [HttpGet]
+        [Route("motogpnews")]
+        [Route("motogpnews/{numberOfRecords}/{numberOfDays}")]
+        public async Task<IActionResult> GetMotoGPNews(int numberOfRecords = 0, int numberOfDays = 0)
+        {
+            string cacheKey = "MotoGPNews" + numberOfRecords + numberOfDays;
+            if (!_cache.TryGetValue(cacheKey, out List<Item> result))
+            {
+                string currentDirectory = Directory.GetCurrentDirectory();
+                IEnumerable<string> feeds = System.IO.File.ReadAllLines(currentDirectory + @"\Feeds\MotoGPNews.txt");
+
+                result = await RSSReader.ReadRSSFeeds(currentDirectory, "MotoGP", feeds) ?? new();
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = CacheDuration
+                };
+
+                _cache.Set(cacheKey, result, cacheEntryOptions);
+            }
+            var orderedResult = result?.OrderByDescending(o => o.PubDate).AsEnumerable();
+
+            if (numberOfRecords > 0)
+                orderedResult = orderedResult?.Take(numberOfRecords);
+            else if (numberOfDays > 0)
+                orderedResult = orderedResult?
+                    .Where(o => o.PubDate > DateTime.Now - new TimeSpan(numberOfDays, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second));
+            else
+                orderedResult = orderedResult?.Take(10);
+
+            return Ok(new Dictionary<string, IEnumerable<Item>> { { "MotoGPNews", orderedResult ?? new List<Item>() } });
         }
 
         [HttpGet]
